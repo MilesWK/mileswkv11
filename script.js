b94235d1617ea3aaffd401ac17275dbaa83af2f9
MilesWK <mileswkrueger@gmail.com>
2026-05-20 09:25:12 -0400

    Add custom cursor with hover effects

---
+++ script.js
@@ -0,0 +1,33 @@
+
+const cursor = document.getElementById('mousey');
+
+window.addEventListener('mousemove', (e) => {
+    gsap.to(cursor, {
+        x: e.clientX,
+        y: e.clientY,
+        duration: 0.5,
+        stagger: 0.2,
+        ease: "back.out"
+    });
+});
+
+
+document.querySelectorAll('.hoverable').forEach((el) => {
+
+    el.addEventListener('mouseenter', () => {
+        gsap.to(cursor, {
+            scale: 3,
+            duration: 0.2,
+            ease: 'back.out',
+        });
+    });
+
+    el.addEventListener('mouseleave', () => {
+        gsap.to(cursor, {
+            scale: 1,
+             duration: 0.2,
+            ease: 'back.out',
+        });
+    });
+
+});
