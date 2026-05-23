f3b607fa80b8fb524673b570b4bdf91d83868f22
MilesWK <mileswkrueger@gmail.com>
2026-05-23 16:45:24 -0400

    Add theme changer and detector, change project hover color

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-
 const cursor = document.getElementById('mousey');
+const themetoggle = document.getElementById('theme')
+const body = document.getElementById('body')
+const themechangeanimation = gsap.timeline({ paused: true });
 
 window.addEventListener('mousemove', (e) => {
     gsap.to(cursor, {
@@ -27,9 +29,37 @@ document.querySelectorAll('.hoverable').forEach((el) => {
         gsap.to(cursor, {
             scale: 1,
              duration: 0.2,
-            ease: 'back.out',
+            ease: 'back.inOut',
         });
         cursor.classList.remove('hover')
     });
 
 });
+
+
+themechangeanimation.to("#sun", { duration: 1, ease: "elastic",  easeReverse: true, morphSVG: "#moon" });
+
+function toLight() {
+    body.classList.remove("dark");
+    body.classList.add("light")
+    themechangeanimation.reverse()
+    themechangeanimation.paused()
+}
+
+function toDark() {
+    body.classList.remove("light");
+    body.classList.add("dark");
+    themechangeanimation.play()
+}
+
+if (window.matchMedia('(prefers-color-scheme: dark)').matches === true) {
+    toDark()
+}
+
+function changeTheme()  {
+    if (body.classList.contains("light")) {
+        toDark()
+    } else {
+        toLight()
+    }
+}
